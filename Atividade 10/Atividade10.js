99155cd17a5a181d5646fc2743ce6d158825cd30
Nicole Baraldi <79142496+NicoleBaraldi@users.noreply.github.com>
2022-05-09 21:21:31 -0300

    Add files via upload

---
+++ Atividade 10/Atividade10.js
@@ -0,0 +1,22 @@
+var aluno = new Object();
+aluno.nome = "Nicole";
+aluno.ra = "0030482021025";
+
+
+var aluno1 = {};
+aluno1.nome = "Nicole";
+aluno1.ra = "0030482021025";
+
+var aluno2 = {
+    nome: "Nicole",
+    ra: "0030482021025"
+}
+
+alert(aluno.nome);
+alert(aluno.ra);
+
+alert(aluno1.nome);
+alert(aluno1.ra);
+
+alert(aluno2.nome);
+alert(aluno2.ra);
